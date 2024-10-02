254d484d617e5e9772acd0af680b3fe6e9d34476
Leon Schmidt <leon18058r@gmail.com>
2024-10-02 23:03:55 +0200

    Refactor api.php to add new routes for retrieving random playlists and checking if a song is in a playlist

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SpotifyDataController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/spotify/random-playlists-and-song', [SpotifyDataController::class, 'getRandomPlaylistsAndSong']);
+    Route::get('/spotify/is-song-in-playlist', [SpotifyDataController::class, 'isSongInPlaylist']);
 });

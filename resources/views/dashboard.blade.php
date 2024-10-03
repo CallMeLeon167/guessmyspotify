210796f7ab151338efd088100c4d77507eefa804
Leon Schmidt <leon18058r@gmail.com>
2024-10-03 17:20:05 +0200

    Refactor CSS to add flash message styles and animations

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,32 +12,8 @@
 </head>
 
 <body>
-    <header>
-        <nav class="container">
-            <a href="/" class="logo">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    width="24" height="24" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z" />
-                </svg>
-                Spotify Playlist-Rätsel
-            </a>
-            @if (Auth::check())
-                <a href="{{ route('logout') }}" class="btn btn-nav">
-                    <img src="{{ $user->image }}" alt="User PFP" class="user-pfp">
-                    Abmelden
-                </a>
-            @else
-                <a href="{{ route('login') }}" class="btn btn-nav"><svg xmlns="http://www.w3.org/2000/svg"
-                        fill="none" height="24" width="24" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
-                    </svg>
-                    Anmelden</a>
-            @endif
-        </nav>
-    </header>
+    @include('includes.flash-message')
+    @include('includes.header')
 
     <main class="container">
         <section id="home" class="fade-in">
